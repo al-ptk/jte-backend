713b4dd86dfb3e6abb92982b62ac28a917c9b04a
Alan Patrick <alan.patrick.fv@gmail.com>
2023-01-24 17:34:39 -0300

    feat: create mongoose basic connection

--- app.js
+++ app.js
@@ -2,6 +2,14 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 
+const mongoose = require('mongoose');
+mongoose.connect(process.env.DB_CONNECTION, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
 app.get('/', (req, res) => {
   res.send('We doing it live');
 });
