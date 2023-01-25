5ee98de9d63da133d15e0257b8c7705c433b62a7
Alan Patrick <alan.patrick.fv@gmail.com>
2023-01-25 09:31:15 -0300

    feat: Create User model

--- app.js
+++ app.js
@@ -1,17 +1,15 @@
+// Set up project 
 require('dotenv').config();
 const express = require('express');
 const app = express();
 
-const mongoose = require('mongoose');
-mongoose.connect(process.env.DB_CONNECTION, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+// Set up mongoose
+require('./mongoose-connection')
 
+// Set up routes
 app.get('/', (req, res) => {
   res.send('We doing it live');
 });
 
+// Export app
 module.exports = app;
---
+++ mongoose-connection.js
@@ -0,0 +1,7 @@
+const mongoose = require('mongoose');
+mongoose.connect(process.env.DB_CONNECTION, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
