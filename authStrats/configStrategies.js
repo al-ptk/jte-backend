f5e6a3aecad9c4c23a2521016b153a75771bd299
Alan Patrick <alan.patrick.fv@gmail.com>
2023-01-26 15:46:21 -0300

    fix: Delete sign up strat file
    No point in using a strat for sign-up because there are more data than
    username and password to create a user registry. I can just do it all
    myself instead.

--- authStrats/configStrategies.js
+++ authStrats/configStrategies.js
@@ -2,12 +2,10 @@ const passport = require('passport');
 
 // Import the strategies
 const signInStrat = require('./signInStrat');
-const signUpStrat = require('./signUpStrat');
 const validateJwtStrat = require('./validateJwtStrat');
 
 // Config the strategies
 passport.use('sign-in', signInStrat);
-passport.use('sign-up', signUpStrat);
 passport.use('validate-jwt', validateJwtStrat);
 
 module.exports = passport;
--- routes/authRouter.js
+++ routes/authRouter.js
@@ -1,22 +1,24 @@
 const route = require('express').Router();
 const passport = require('../authStrats/configStrategies');
 const jwt = require('jsonwebtoken');
+const User = require('../models/User');
 
 // Sign In Route
 route.post('/sign-in', async (req, res, next) => {
-  // On valid sign-in, send JWT
   passport.authenticate('sign-in', async (err, user, info) => {
     try {
+      // Escape if any errors
       if (err || !user) {
         const error = new Error('An error occurred.');
         return next(error);
       }
 
+      // On valid sign-in, send JWT
       req.login(user, { session: false }, async (error) => {
         if (error) return next(error);
         const body = { _id: user._id, name: user.name };
         const token = jwt.sign({ user: body }, process.env.JWT_KEY);
-        return res.json({ body, token });
+        return res.json({ token });
       });
     } catch (error) {
       return next(error);
@@ -25,31 +27,35 @@ route.post('/sign-in', async (req, res, next) => {
 });
 
 // Sign Up Route
-// route.post('/sign-up', async (req, res, next) => {
-//   try {
-//     // Get user credentials
-//     const { name, email, password } = req.body;
-//     // Check if request body is appropriate
-//     if (!name || !email || !password) {
-//       res.send('Ill formed request body');
-//     }
-//     // Refuse sign-up if email already registered
-//     const userExists = await User.findOne({ email }).exec();
-//     if (userExists) {
-//       return res.status(401).send('User already exists');
-//     }
-//     // Create new user
-//     const newUser = new User({
-//       name,
-//       email,
-//       password, // the hashing is done in the save pre-hook on the Schema
-//     });
-//     // Commit new user
-//     await newUser.save();
-//     res.send('User registered!');
-//   } catch (err) {
-//     return next(err);
-//   }
-// });
+route.post('/sign-up', async (req, res, next) => {
+  try {
+    // Get user credentials
+    const { name, email, password } = req.body;
+
+    // Check if request body is appropriate
+    if (!name || !email || !password) {
+      res.send('Ill formed request body');
+    }
+
+    // Refuse sign-up if email already registered
+    const userExists = await User.findOne({ email }).exec();
+    if (userExists) {
+      return res.status(401).send('User already exists');
+    }
+
+    // Create new user
+    const newUser = new User({
+      name,
+      email,
+      password, // the hashing is done in the save pre-hook on the Schema
+    });
+
+    // Commit new user
+    await newUser.save();
+    res.json({ message: 'user registered' });
+  } catch (err) {
+    return next(err);
+  }
+});
 
 module.exports = route;
