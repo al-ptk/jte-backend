bfa56dd25559ad1693b99c1bb554fa5a11f1de7c
Alan Patrick <alan.patrick.fv@gmail.com>
2023-01-30 16:30:59 -0300

    feat: add controller to projects router

--- routes/projectsRouter.js
+++ routes/projectsRouter.js
@@ -1,11 +1,12 @@
 const route = require('express').Router();
 const passport = require('../authStrats/configStrategies');
+const projectControler = require('../controllers/projectControllers');
 
 // Middleware to authenticate users
-// route.use(passport.authenticate('validate-jwt', { session: false }));
+route.use(passport.authenticate('validate-jwt', { session: false }));
 
 // Routes for managing projects
-route.get('/projects', mockFunction);
+route.get('/projects', projectControler.getAllProjects);
 
 route.get('/project/:projectId', mockFunction);
 
